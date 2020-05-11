07a1da54f6a7f2eb8abb860795ef54715c518352
bip63 <60354591+bip63@users.noreply.github.com>
2020-05-11 17:39:01 -0400

    Create twitter_credentials.py

---
+++ Final_Project/twitter_credentials.py
@@ -0,0 +1,7 @@
+# The source code used to build this project can be found here: https://github.com/vprusso/youtube_tutorials/tree/master/twitter_python
+# Variables that contain the user credentials to access Twitter API.
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
